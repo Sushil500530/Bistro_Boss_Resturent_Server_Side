c5e0f5bfe42f41b085bfda4834aceb37a74baff4
Sushil97 <sushil500530@gmail.com>
2023-11-15 20:34:37 +0600

    add admin data for user with update and delet data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -26,10 +26,74 @@ async function run() {
     // await client.connect();
     await client.db("admin").command({ ping: 1 });
 
+    const userCollection = client.db('bistroDB').collection('users');
     const menuCollection = client.db('bistroDB').collection('menu');
     const reviewCollection = client.db('bistroDB').collection('reviews');
     const cartCollection = client.db('bistroDB').collection('carts');
 
+    // user related api 
+    app.get('/users', async(req,res) => {
+      try{
+        const result = await userCollection.find().toArray();
+        res.send(result)
+      }
+      catch(error){
+        console.log(error);
+      }
+    })
+    app.post('/users', async(req,res) => {
+      try{
+        const user = req.body;
+        // insert email if user doesn't exiest
+        // you can do this many wasy 
+        // 1..email unique
+        // 2.. upsert
+        // 3.. simple checking
+        const query = {email: user.email};
+        const existingUser = await userCollection.findOne(query);
+        if(existingUser){
+          return res.send({message : 'user already exists', insertedId:null})
+        }
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+      }
+      catch(error){
+        console.log(error);
+      }
+    })
+
+    app.patch('/users/admin/:id', async(req,res) => {
+      try{
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)};
+        const updatedDoc = {
+          $set: {
+            role: 'admin'
+          }
+        }
+        const result = await userCollection.updateOne(filter,updatedDoc);
+        res.send(result)
+
+      }
+      catch(err){
+        console.error(err);
+       }
+    })
+
+    app.delete('/users/:id', async(req,res) => {
+     try{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+     }
+     catch(err){
+      console.error(err);
+     }
+    })
+
+
+    // menu related api 
     app.get('/menu', async (req, res) => {
       try {
         const result = await menuCollection.find().toArray()
@@ -53,7 +117,7 @@ async function run() {
     app.get('/carts', async (req, res) => {
       try {
         const email = req.query.email;
-        const query = {email:email}
+        const query = { email: email }
         const result = await cartCollection.find(query).toArray();
         res.send(result);
       }
@@ -73,7 +137,19 @@ async function run() {
       }
     })
 
+    app.delete('/carts/:id', async (req, res) => {
+      try {
+        const id = req.params.id;
+        console.log(id);
+        const query = {_id: new ObjectId(id)}
+        const result = await cartCollection.deleteOne(query);
+        res.send(result);
 
+      }
+      catch (err) {
+        console.log(err);
+      }
+    })
 
 
 
